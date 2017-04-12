9b1abc199e1ddd616c56d460e8b8739eb31141fb
Balthair94 <archerbaltazar@gmail.com>
2017-04-12 00:08:19 -0500

    It is possible to share text

--- app/src/main/java/baltamon/mx/appsharecontent/MainActivity.java
+++ app/src/main/java/baltamon/mx/appsharecontent/MainActivity.java
@@ -1,10 +1,15 @@
 package baltamon.mx.appsharecontent;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setToolbar();
+
+        setButtons();
+    }
+
+    private void setButtons() {
+        Button shareButton = (Button) findViewById(R.id.btn_share);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareText();
+            }
+        });
+    }
+
+    public void shareText(){
+        EditText editText = (EditText) findViewById(R.id.et_content);
+
+        if (!editText.getText().toString().equals("")){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        } else
+            Toast.makeText(this, "You need to include a text", Toast.LENGTH_SHORT).show();
     }
 
     private void setToolbar(){
@@ -24,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null){
-            actionBar.setTitle("Share content - share text");
+            actionBar.setTitle("Share content");
         }
     }
 }
